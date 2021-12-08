96eecbd9106062f9a2f4a80c6ae8ad36216ea1b5
nat-1501 <natalisoares90@gmail.com>
2021-12-07 22:42:51 -0300

    Utilizando js para abrir e fechar o menu hamburguer

---
+++ script.js
@@ -0,0 +1,5 @@
+var hamburguer = document.querySelector(".hamburguer");
+
+hamburguer.addEventListener("click", function(){
+    document.querySelector(".sidebar").classList.toggle("show-menu");
+});
